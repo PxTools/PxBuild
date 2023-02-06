eb948c25e5377b092890574cf8137ec8c5d5fab7
Johannes <jfi@ssb.no>
2023-02-06 09:01:29 +0100

    namedtuple with hardcoded cols. for intelisence to work

--- SpecReader.py
+++ SpecReader.py
@@ -1,16 +1,23 @@
 import csv
 from collections import namedtuple
 
+SpecRow = namedtuple("SpecRow", ['px_keyword', 'is_lang_dependent', 'is_Mandatory', 'px_SubKey', 'is_SubKey_Optional', 
+'is_duplicate_keypart_allowed_', 'px_valuetype', 'px_enumvalues', 'px_comment'] )
+
 with open("Keywords.csv", "r",encoding="utf-8-sig") as theSpecCsv:
     reader = csv.reader(theSpecCsv,delimiter=";" )
     header = next(reader)
     print(header)
-    SpecRow = namedtuple("SpecRow", header)
+   # SpecRow = namedtuple("SpecRow", header)
+
     data = [SpecRow(*row) for row in reader]  
 
+    print(data[2].px_keyword)
 
-
-print("\n",data[3])
+dataDict = {}
+for row2 in data:
+    dataDict[row2.px_keyword] = row2
+     
 
 
 
