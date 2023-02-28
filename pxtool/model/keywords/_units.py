c78cc44e363aaa5b5329e45e79f6496b0ed478ac
Johannes <jfi@ssb.no>
2023-02-28 16:33:32 +0100

    subkey is also optional for units

--- pxtool/model/keywords/_units.py
+++ pxtool/model/keywords/_units.py
@@ -9,7 +9,7 @@ class _PX_UNITS(_PXValueByKey):
     is_language_dependent:bool = True
 
 
-    def set(self, units:str, content:str, lang:str = None) -> None:
+    def set(self, units:str, content:str=None, lang:str = None) -> None:
         """  """
         LineValidator.is_not_None( self._keyword, units)
         LineValidator.is_string( self._keyword, units)
