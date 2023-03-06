6aebad9cba5f53798ce18eaf21ba7ef64b0c74bf
Johannes <jfi@ssb.no>
2023-03-06 13:29:23 +0100

    Adjusted to Moved ValidatePx to the same folder as PXFileModel , and added Cleaner there

--- demo/loader.py
+++ demo/loader.py
@@ -3,13 +3,13 @@ my = sys.path[0].replace("\\demo","\\")
 sys.path.insert(1,my)
 
 from pxtool.loaders.loader_pxfile import Loader
-from pxtool.model.util.util import Util
+from pxtool.model.cleaner import Cleaner
 
 #loader = Loader('testdata/testdataKort.px')
 #loader = Loader('testdata/statfin_khi_pxt_11xm.px')
 loader = Loader('testdata/statfin_khi_pxt_11xm_full.px')
 
-Util.apply_default_language(loader.outModel)
+Cleaner.apply_default_language(loader.outModel)
 
 with open('output.txt', 'w') as f:
     print(loader.outModel, file=f)
