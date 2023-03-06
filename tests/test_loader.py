5f7ac52f4e315ebfdde3bd0773eaba2d0643435d
Johannes <jfi@ssb.no>
2023-03-06 14:05:54 +0100

    Moved Bad test-files into separate folder

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,15 +2,15 @@ from pxtool.loaders.loader_pxfile import Loader
 import pytest
 
 def test_BadStart1_file_raises_value_error():
-    with pytest.raises(ValueError):
-        dummy = Loader('testdata/BadStart1.px')
+    with pytest.raises(ValueError, match="A PxFile must start with a letter"):
+        dummy = Loader('testdata/BadFiles/BadStart1.px')
 
 def test_BadStart2_file_raises_value_error():
-    with pytest.raises(ValueError):
-        dummy = Loader('testdata/BadStart2.px')  
+    with pytest.raises(ValueError, match="A PxFile must start with a letter"):
+        dummy = Loader('testdata/BadFiles/BadStart2.px')  
 
 def test_BadStart3_file_raises_value_error():
-    with pytest.raises(ValueError):
-        dummy = Loader('testdata/BadStart3.px')    
+    with pytest.raises(ValueError, match="A PxFile must start with a letter"):
+        dummy = Loader('testdata/BadFiles/BadStart3.px')    
 
 
