988cab6b8421d62f278c59a7c3e30ca69251e304
Johannes <jfi@ssb.no>
2023-05-05 16:56:25 +0200

    adjusted check_showdecimals to desimals beeing mandatory.

--- pxtool/operations_on_model/validator/checks/check_showdecimals.py
+++ pxtool/operations_on_model/validator/checks/check_showdecimals.py
@@ -6,7 +6,7 @@ import pxtool.model.util.constants as const
    
 def check_showdecimals(model:PXFileModel) -> ValidationResult:
         val_result = ValidationResult(desc="Check if value for showdecimals is greater than value for decimals")
-        if model.decimals.has_value() and model.showdecimals.has_value():
+        if model.showdecimals.has_value():
             if(model.showdecimals.get_value() > model.decimals.get_value()):
                 val_result.add_error(f"Value <{model.showdecimals.get_value()}> in showdecimals is not valid. The value must be less or equal decimals.")
         return val_result
