60bdcd6427ba75f2b77b7a2e923eefaab98c038b
Johannes <jfi@ssb.no>
2023-05-05 18:37:22 +0200

    cleaner

--- pxtool/operations_on_model/validator/checks/check_lang_keys.py
+++ pxtool/operations_on_model/validator/checks/check_lang_keys.py
@@ -5,11 +5,10 @@ import pxtool.model.util.constants as const
 
 def check_lang_keys(model:PXFileModel) -> ValidationResult:
         val_result = ValidationResult(desc="Check if all language keys have a valid language")
-        if model.languages.has_value():
-            for key in const.LANGDEPENDENT_KEYWORDS:
+        for key in const.LANGDEPENDENT_KEYWORDS:
                 keyword = model.get_attribute(key)
                 if keyword.has_value():
                     for lang_key in keyword._value_by_key:
-                        if not lang_key.lang in model.languages._px_value.list_of_strings:
+                        if not lang_key.lang in model.languages.get_value():
                             val_result.add_error(f"Specified language code \"{lang_key.lang}\" for keyword {keyword._keyword} must be one of the codes in keyword languages: {model.languages._px_value}")
         return val_result
