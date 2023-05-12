878467316229c78c836b1e593f05bcd990439403
PerIngeVaaje <per.inge.vaaje@ssb.no>
2023-05-12 15:07:22 +0200

    Added check and modified validation for contentsvariable

--- pxtool/operations_on_model/validator/checks/check_contentsvariable_is_present.py
+++ pxtool/operations_on_model/validator/checks/check_contentsvariable_is_present.py
@@ -5,13 +5,14 @@ import pxtool.model.util.constants as const
 def check_contentsvariable_is_present(model:PXFileModel) -> ValidationResult:
     val_result = ValidationResult(desc="Check that contentsvariable is present and placed in same position for all languages.")
 
-    if len(model.contvariable) < 1 :
-        val_result.add_error(f"Contentsvariable does not exist. It must be specified.")
-        return  val_result   
-    cont_variable_name = model.contvariable
-    first_pass = True 
+    for langu in model.languages.get_value():
+        try:
+            cont_value = model.contvariable.get_value(langu)
+        except:    
+            val_result.add_error(f"Value for Contentsvariable does not exist for for language code: {langu}. It must be specified.")
+            return  val_result   
     
-
+    first_pass = True 
     for langu in model.languages.get_value():
         stub_and_head = model.stub.get_value(langu) +  model.heading.get_value(langu) 
         try:
---
+++ tests/operations_on_model/validator/checks/test_check_contentsvariable_is_present.py
@@ -0,0 +1,52 @@
+from pxtool.model.px_file_model import PXFileModel
+from pxtool.operations_on_model.validator.checks.check_contentsvariable_is_present import check_contentsvariable_is_present
+import pytest
+
+def test_contentsvariable_is_ok_both_languages_lang():
+    pxfile = PXFileModel()
+    pxfile.languages.set(["no","en"])
+    pxfile.contvariable.set("statvar","no")
+    pxfile.contvariable.set("contents","en")
+    pxfile.stub.set(["var_no_1","var_no_2","var_no_3"], "no")
+    pxfile.stub.set(["var_en_1","var_en_2","var_en_3"], "en") 
+    pxfile.heading.set(["var_no_4","statvar","var_no_5"], "no")
+    pxfile.heading.set(["var_en_4","contents","var_en_5"], "en") 
+    val_rep = check_contentsvariable_is_present(pxfile)
+    assert val_rep.is_valid == True
+    
+def test_contentsvariable_is_missing_first_lang():
+    pxfile = PXFileModel()
+    pxfile.languages.set(["no","en"])
+    pxfile.contvariable.set("contents","en")
+    pxfile.stub.set(["var_no_1","var_no_2","var_no_3"], "no")
+    pxfile.stub.set(["var_en_1","var_en_2","var_en_3"], "en") 
+    pxfile.heading.set(["var_no_4","statvar","var_no_5"], "no")
+    pxfile.heading.set(["var_en_4","contents","var_en_5"], "en") 
+    val_rep = check_contentsvariable_is_present(pxfile)
+    assert val_rep.is_valid == False
+    assert val_rep.error_msg == "Value for Contentsvariable does not exist for for language code: no. It must be specified."
+    
+def test_contentsvariable_is_missing_second_lang():
+    pxfile = PXFileModel()
+    pxfile.languages.set(["no","en"])
+    pxfile.contvariable.set("statvar","no")
+    pxfile.stub.set(["var_no_1","var_no_2","var_no_3"], "no")
+    pxfile.stub.set(["var_en_1","var_en_2","var_en_3"], "en") 
+    pxfile.heading.set(["var_no_4","statvar","var_no_5"], "no")
+    pxfile.heading.set(["var_en_4","contents","var_en_5"], "en") 
+    val_rep = check_contentsvariable_is_present(pxfile)
+    assert val_rep.is_valid == False
+    assert val_rep.error_msg == "Value for Contentsvariable does not exist for for language code: en. It must be specified."
+
+def test_contentsvariable_position_doent_match():
+    pxfile = PXFileModel()
+    pxfile.languages.set(["no","en"])
+    pxfile.contvariable.set("statvar","no")
+    pxfile.contvariable.set("contents","en")
+    pxfile.stub.set(["var_no_1","var_no_2","var_no_3"], "no")
+    pxfile.stub.set(["var_en_1","var_en_2","var_en_3"], "en") 
+    pxfile.heading.set(["statvar","var_no_4","var_no_5"], "no")
+    pxfile.heading.set(["var_en_4","contents","var_en_5"], "en") 
+    val_rep = check_contentsvariable_is_present(pxfile)
+    assert val_rep.is_valid == False
+    assert val_rep.error_msg == "Position of contentsvariable does not matchs in HEADING or STUB for language code: en and no."
