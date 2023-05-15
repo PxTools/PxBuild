8f0e5526f18e2f86e1665f69c1ea013da17d6914
Johannes <jfi@ssb.no>
2023-05-15 13:41:11 +0200

    [STATBANK-353] Completeness removed unused code

--- pxtool/operations_on_model/validator/checks/check_completeness.py
+++ pxtool/operations_on_model/validator/checks/check_completeness.py
@@ -118,72 +118,7 @@ class _Checker:
 
 
 
-                
-
-
-
-
-    def check_keytype_values(self, key:_KeytypeValuesLangMulti, keyword:_PxValueByKey) -> None:  
-        if not key.values:
-            if not keyword.subkey_optional:
-                self.val_result.add_error(f"{self.error_intro}: Values can not be None. For lang:{key.lang}.")
-        else:
-            dimensions = self.model.stub.get_value(key.lang) + self.model.heading.get_value(key.lang)
-            if not len(dimensions) == len(key.values):
-                   self.val_result.add_error(f"{self.error_intro}: There are {len(dimensions)} dimensions, but {len(key.values)} values. For lang:{key.lang}.")
-            else:
-                dimension_cnt=-1
-                for value in key.values:
-                    dimension_cnt += 1
-                    if value == "*":
-                        continue
-                    self.check_if_value_in_values(key.lang,dimensions[dimension_cnt],value,self.model)
-
-
-
-
-    def check_keytype_variable(self, key:_KeytypeVariableLang, keyword:_PxValueByKey) -> None:                           
-        if not key.variable:
-            if not keyword.subkey_optional:
-                self.val_result.add_error(f"{self.error_intro}: Variable can not be None. For lang:{key.lang}.")
-        else:
-            self.check_variable_in_stub_or_heading(key.lang, key.variable, self.model)
-
-    def check_keytype_variable_value(self, key:_KeytypeVariableValueLang, keyword:_PxValueByKey) -> None:   
-        if not key.variable:
-            if key.value:
-                self.val_result.add_error(f"{self.error_intro}: Found value, but no variable . For lang:{key.lang}.")
-            if not keyword.subkey_optional:
-                self.val_result.add_error(f"{self.error_intro}: Variable can not be None. For lang:{key.lang}.")
-        else:
-            if not key.value:
-                if not keyword.subkey_optional:
-                    self.val_result.add_error(f"{self.error_intro}: Need value for variable {key.variable}. For lang:{key.lang}.")        
-            else:
-                if self.check_variable_in_stub_or_heading(key.lang, key.variable, self.model):
-                    self.check_if_value_in_values(key.lang, key.variable, key.value, self.model)   
-
-
-    def check_keytype_content(self, key:_KeytypeContentLang, keyword:_PxValueByKey) -> None:                        
-        if not key.content:
-            if not keyword.subkey_optional:
-                self.val_result.add_error(f"{self.error_intro}: Content value can not be None. For lang:{key.lang}.")
-        else:
-            self.check_if_value_in_values(key.lang, self.model.contvariable.get_value(key.lang), key.content, self.model)                                       
-                                      
-
-    def check_if_value_in_values(self, lang:str, dimension:str, value:str, model:PXFileModel) -> bool:
-        my_out = value in model.values.get_value(dimension,lang)
-        if not my_out :
-            self.val_result.add_error(f"{self.error_intro}: Cannot find item {value} in VALUES for vaiable:{dimension} and lang:{lang}.")
-        return my_out
-
-    def check_variable_in_stub_or_heading(self, lang:str, variable:str, model:PXFileModel) -> bool:
-        my_out = variable in model.stub.get_value(lang) + model.heading.get_value(lang)
-        if not my_out :
-            self.val_result.add_error(f"{self.error_intro}: Cannot find variable {variable} in stub + heading. For lang:{lang}.")
-        return my_out
-
+ 
 
 def check_completeness(model:PXFileModel) -> ValidationResult:
     return _Checker(model).val_result
