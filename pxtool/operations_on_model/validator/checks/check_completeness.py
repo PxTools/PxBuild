4a0b7170769cc312738625ed715a4515eb41bebf
Johannes <jfi@ssb.no>
2023-05-15 13:39:02 +0200

    [STATBANK-353] All CompletenessTypes

--- pxtool/operations_on_model/validator/checks/check_completeness.py
+++ pxtool/operations_on_model/validator/checks/check_completeness.py
@@ -13,13 +13,13 @@ class _Checker:
         self.all_languages = model.languages.get_value()
 
         self.indexByLangAndVariable = {}
+        self.variables = {} 
         for lang in self.all_languages:
                 index=-1
-                variables = self.model.stub.get_value(lang) + self.model.heading.get_value(lang)            
-                for variable in variables:
+                self.variables[lang] = self.model.stub.get_value(lang) + self.model.heading.get_value(lang)            
+                for variable in self.variables[lang]:
                     index += 1
                     self.indexByLangAndVariable[(lang,variable)] = index
-
         
         #listOfOtherVariableNamesByVariableNameOfFirstLanguage
 
@@ -34,10 +34,27 @@ class _Checker:
 
         for keyword_name in const.KEYWORDS_PYTHONIC_MAP:
             keyword = model.get_attribute(const.KEYWORDS_PYTHONIC_MAP[keyword_name])
-            if keyword.is_present() and keyword.completeness:
+            if keyword.is_present() and keyword.completeness_type:
                 self.error_intro = f"For keyword {keyword._keyword}"
-                if keyword.completeness == "Lang":
+
+                if keyword.completeness_type == "Lang":
                     self.check_completeness_lang(keyword)
+                elif keyword.completeness_type == "AllContent":
+                    self.check_completeness_all_content(keyword)
+                elif keyword.completeness_type == "EachVariable":
+                    self.check_completeness_each_variable(keyword)    
+                elif keyword.completeness_type == "OneVariable":
+                    self.check_completeness_one_variable(keyword)  
+                elif keyword.completeness_type == "AllVariables":
+                    self.check_completeness_all_variables(keyword)
+                elif keyword.completeness_type == "EachVarVal":
+                    self.check_completeness_each_var_value(keyword)
+                elif keyword.completeness_type == "X":
+                    pass
+                elif keyword.completeness_type == "TODO":
+                   self.val_result.add_error(f"{self.error_intro}:Sorry keyword not supported yet.")
+                else: # pragma: no cover
+                    raise Exception(f"{self.error_intro}:Sorry, unknown completeness_type:{keyword.completeness_type}")                                     
 
 
 
@@ -46,14 +63,62 @@ class _Checker:
             if not keyword.has_value(lang):
                 self.val_result.add_error(f"{self.error_intro}:Missing value for lang:{lang}")
 
-
-    def check_keytype_lang_with_subkey(self, keyword:_PxValueByKey) -> None:
-        
+    def check_completeness_each_variable(self, keyword:_PxValueByKey) -> None:
         for key in keyword._value_by_key:
+            all_tuples_same_variable_index =  [k for k, v in self.indexByLangAndVariable.items() if v == self.indexByLangAndVariable[(key.lang,key.variable)]]              
+            for one_tuple in all_tuples_same_variable_index:
+                if key.lang == one_tuple[0]:
+                    continue
+                if not keyword.has_value(one_tuple[1],one_tuple[0]):
+                    self.val_result.add_error(f"{self.error_intro}:Missing value for variable:{one_tuple[1]} and lang:{one_tuple[0]}")
+
+    def check_completeness_each_var_value(self, keyword:_PxValueByKey) -> None:
+        for key in keyword._value_by_key:
+            vari_pos = self.variables[key.lang].index(key.variable)
+            value_pos  = self.model.values.get_value(key.variable,key.lang).index(key.value)
+
             for lang in self.all_languages:
-                if not lang == key.lang:
-                   if not keyword.has_value():
-                       pass  
+                if lang == key.lang:
+                    continue
+                
+                vari_name = self.variables[lang][vari_pos]
+                value_name = self.model.values.get_value(vari_name,lang)[value_pos]
+                if not keyword.has_value(vari_name, value_name, lang):
+                    self.val_result.add_error(f"{self.error_intro}:Missing value for variable:{vari_name},value: {value_name} and lang:{lang}")
+
+    def check_completeness_all_variables(self, keyword:_PxValueByKey) -> None:
+        for lang in self.all_languages:
+            for vari in self.variables[lang]:        
+                if not keyword.has_value(vari, lang):
+                    self.val_result.add_error(f"{self.error_intro}:Missing value for variable:{vari} and lang:{lang}")                 
+
+    def check_completeness_one_variable(self, keyword:_PxValueByKey) -> None:
+        _seen_languages={}
+        theOneIndex = ""
+        for key in keyword._value_by_key:
+            _seen_languages[key.lang]=1
+
+            tmpIndex = self.indexByLangAndVariable[(key.lang,key.variable)]
+            if not theOneIndex:
+                theOneIndex = tmpIndex
+            else:
+                if not tmpIndex == theOneIndex:
+                    self.val_result.add_error(f"{self.error_intro}: Should only reference 1 variable. Found 2: index {theOneIndex} and {tmpIndex}.")
+        
+        for lang in self.all_languages:
+            if not lang in _seen_languages:
+                self.val_result.add_error(f"{self.error_intro}:Missing value for lang:{lang}")        
+
+
+    def check_completeness_all_content(self, keyword:_PxValueByKey) -> None:
+            for lang in self.all_languages:
+                for content in self.model.values.get_value(self.model.contvariable.get_value(lang),lang):
+                    if not keyword.has_value(content, lang):
+                        self.val_result.add_error(f"{self.error_intro}:Missing value for content:{content} and lang:{lang}")
+
+
+
+                
 
 
 
