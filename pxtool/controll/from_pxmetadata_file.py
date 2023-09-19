0dbc0c2a23ac45f64a99398222ec092e9995efff
Johannes <jfi@ssb.no>
2023-09-19 17:11:37 +0200

    [STATBANK-390] added title ...

--- pxtool/controll/from_pxmetadata_file.py
+++ pxtool/controll/from_pxmetadata_file.py
@@ -11,6 +11,9 @@ from pxtool.models.output.pxfile.px_file_model import PXFileModel
 from .datafile import ParquetDatasource
 
 class LoadFromPxmetadata():
+   LabelConstructionOptionDict={"LabelConstructionOption.text":0, "LabelConstructionOption.code":1,"LabelConstructionOption.text_code":3, "LabelConstructionOption.code_text":2}
+
+
    def __init__(self, filename: str, sourceType:str) -> None:
       self._filename = filename
       print(self._filename)
@@ -58,8 +61,9 @@ class LoadFromPxmetadata():
       ## loop in languages
       self._current_lang="no" #todo
 
+
       self._stub=[]
-      self._heading=[self._config.contvariable]
+      self._heading=[self._config.contvariable[self._current_lang] ]
       out_model = PXFileModel()
       out_model.language.set(self._current_lang)
 
@@ -70,6 +74,9 @@ class LoadFromPxmetadata():
 
       if self._pxmetadata_model.dataset.coded_dimensions:
         for my_var in self._pxmetadata_model.dataset.coded_dimensions: 
+          
+
+
           my_funny_var_id = my_var.label[self._current_lang]
           self._stub.append(my_funny_var_id)
           my_var_code = my_var.code if my_var.code is not None else my_var.column_name
@@ -85,13 +92,23 @@ class LoadFromPxmetadata():
 
           out_model.values.set(out_values,my_funny_var_id,self._current_lang)   
           out_model.codes.set(out_codes,my_funny_var_id,self._current_lang) 
-          
+          if my_var.is_geo_variable_type:
+             out_model.variable_type.set("G", my_funny_var_id,self._current_lang) 
+          else:
+             out_model.variable_type.set("N", my_funny_var_id,self._current_lang) 
+
+          out_model.prestext.set( self.LabelConstructionOptionDict[str(my_var.label_construction_option)], my_funny_var_id,self._current_lang) 
+                
         #self.AddPxCodesToPXFileModel(self._pxmetadata_model, out_model)
 
 
 
+      self._contact_string = self.GetContactString(self._pxstatistics)
+      self._last_updated = self.GetLastUpdated(self._pxstatistics) 
+
       if self._pxmetadata_model.dataset.measurements:
-        my_funny_var_id = self._config.contvariable # TODO må ha en no/en dict her 
+        my_funny_var_id = self._config.contvariable[self._current_lang]
+         
         out_codes=[]
         out_values =[]
         for my_cont in self._pxmetadata_model.dataset.measurements: 
@@ -102,6 +119,12 @@ class LoadFromPxmetadata():
           out_model.seasadj.set(my_cont.is_seasonally_adjusted or False,  my_funny_cont_id,self._current_lang)
           out_model.dayadj.set(my_cont.is_workingdays_adjusted or False,  my_funny_cont_id,self._current_lang)
           out_model.units.set(my_cont.unit_of_measure[self._current_lang], my_funny_cont_id,self._current_lang)
+          out_model.contact.set(self._contact_string, my_funny_cont_id,self._current_lang) 
+          out_model.last_updated.set(self._last_updated, my_funny_cont_id,self._current_lang) 
+
+          if my_cont.reference_period[self._current_lang]:
+            out_model.refperiod.set(my_cont.reference_period[self._current_lang], my_funny_cont_id,self._current_lang)
+
 
           if my_cont.show_decimals > 0 :
              out_model.precision.set(my_cont.show_decimals, my_funny_var_id,  my_funny_cont_id,self._current_lang)
@@ -112,7 +135,9 @@ class LoadFromPxmetadata():
              out_model.cfprices.set(str(my_cont.price_type),  my_funny_cont_id,self._current_lang)
 
         out_model.values.set(out_values, my_funny_var_id, self._current_lang)
-        out_model.codes.set(out_codes, my_funny_var_id,self._current_lang) 
+        out_model.codes.set(out_codes, my_funny_var_id,self._current_lang)
+        out_model.variablecode.set(self._config.contvariable_code, my_funny_var_id, self._current_lang) 
+        out_model.variable_type.set("C", my_funny_var_id, self._current_lang) 
 
         
 
@@ -134,17 +159,16 @@ class LoadFromPxmetadata():
      
 
       #its time
-      #
+
       my_periods = self._parquet.GetTimePeriodes(self._pxmetadata_model.dataset.time_dimension.column_name) 
-   #    self._pxmetadata_model.dataset.time_dimension.column_name
-  #    self._pxmetadata_model.dataset.time_dimension.label
 
       my_funny_var_id=self._pxmetadata_model.dataset.time_dimension.label[self._current_lang]
 
       self._heading.append(my_funny_var_id)
       out_model.values.set(my_periods, my_funny_var_id, self._current_lang)
       out_model.codes.set(my_periods, my_funny_var_id,self._current_lang) 
-
+      out_model.variablecode.set(self._config.timevariable_code, my_funny_var_id, self._current_lang)  
+      out_model.variable_type.set("T", my_funny_var_id,self._current_lang)  
       #STUB and or HEADING
       if self._heading:
          out_model.heading.set(self._heading, self._current_lang)
@@ -154,11 +178,28 @@ class LoadFromPxmetadata():
 
       if not self._stub and not self._heading:
          raise Exception("Sorry, both stub and heading are empty.")
-
-
+      
       self.AddPxStatisticsToPXFileModel(self._pxstatistics, out_model)
+
+      lang = self._current_lang
+      model = self._pxmetadata_model.dataset 
+
+      vari_list = self._stub + self._heading 
+
+      tmp_string = ", ".join(vari_list[:-1]) 
+
+      title = model.table_id + ": " + model.base_title[lang] + ", " +self._config.admin.the_word_by[lang]+ " " + tmp_string +" " + self._config.admin.the_word_and[lang] + " "+ vari_list[-1];  
+      
+
+      out_model.title.set(title,self._current_lang)
       print("outmodel:\n",out_model)
 
+
+   def GetContactString(self, in_model:PxStatistics) -> str:
+       return "Todo: get from PxStatistics"
+   
+   def GetLastUpdated(self, in_model:PxStatistics) -> str:
+       return "Todo: get from PxStatistics"
 
    def  AddPxStatisticsToPXFileModel(self, in_model:PxStatistics , out_model:PXFileModel):
       lang=self._current_lang
@@ -172,6 +213,7 @@ class LoadFromPxmetadata():
    def  AddPxMetadataToPXFileModel(self, in_model:PxMetadata , out_model:PXFileModel):
       lang=self._current_lang
       out_model.tableid.set(in_model.dataset.table_id)
+      out_model.matrix.set("tab_"+in_model.dataset.table_id)
       out_model.contents.set( in_model.dataset.table_id + ":" + in_model.dataset.base_title[lang]+",",lang)
 
 
@@ -182,7 +224,7 @@ class LoadFromPxmetadata():
        out_model.codepage.set(str(in_config.code_page))
 
        out_model.descriptiondefault.set((in_config.description_default or False))
-       out_model.contvariable.set(str(in_config.contvariable))
+       out_model.contvariable.set(str(in_config.contvariable[self._current_lang] ))
        out_model.datasymbol1.set(str(in_config.datasymbol1))
        out_model.datasymbol2.set(str(in_config.datasymbol2))
        out_model.datasymbol3.set(str(in_config.datasymbol3))
