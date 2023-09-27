cc5db08a35bc70d3fdc1cc3b0aef2bf3c0e647ae
Johannes <jfi@ssb.no>
2023-09-27 13:21:52 +0200

    added comments

--- pxtool/controll/for_get_data.py
+++ pxtool/controll/for_get_data.py
@@ -1,13 +1,18 @@
 from typing import List
 
 class ForGetData:
+    """ Helper class for data extraction """
+
     def __init__(self, colname_in_dataframe:str,codelist:List[str] ) -> None:
        self._colname_in_dataframe:str = colname_in_dataframe
+       
        self._position_of_value = {string: index for index, string in enumerate(codelist)}
+       """ Dict holding position in codelist as a function of the code """
+
        self._length_of_codelist = len(codelist)
 
        self.factor:int = -1
-
+       """This depends on the desired output order of the variables. Is the (math) product of _length_of_codelist of the previous variables """
 
     def getIndexContrib(self,row) -> int:
         myCode = row[self._colname_in_dataframe]
