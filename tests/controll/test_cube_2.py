acc4c352b17af03462d71a491248e7a3f3e462e8
Johannes <jfi@ssb.no>
2024-01-11 16:35:23 +0100

    Improved TestCube2

--- tests/controll/test_cube_2.py
+++ tests/controll/test_cube_2.py
@@ -1,15 +1,26 @@
 import pytest
-import pxbuild
-from pxbuild.models.output.pxfile.px_file_model import PXFileModel
-#import testdata.expected.test_data_03024 as expected
+import filecmp
+import os
 
+import pxbuild
 
 class TestCube2:
     def test_cube_2_ok(self):
       dummy = pxbuild.LoadFromPxmetadata('2', 'testdata/test_cube_2/test_config.json')
-      
 
-    #assert actual_data == expected.DATA
+      path = "testdata/test_cube_2" 
+      path_expected = path + "/expected/"
+      path_actual = path + "/actual/"
+      filelist = os.listdir(path_expected)
+
+      for file in filelist:
+           print(file)
+
+           file_expected = path_expected + file
+           file_actual = path_actual + file
+           result = filecmp.cmp(file_expected, file_actual, shallow=False)
+
+           assert result, file + " is not as expected."      
 
 
     
